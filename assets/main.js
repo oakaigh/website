fa28bc8e949fb0a05d7c09799f8d5baf458c670c
Jared C <rexjo@homelab.home>
2021-05-20 09:38:21 -0700

    sync

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,25 @@
 'use strict';
 
-function module_check(modules) {
-    for (var name in modules) {
-        var objects = modules[name];
+var modules = {};
+
+function module_args(args, defaults) {
+    return Object.assign({}, defaults, args);
+}
+
+function module_init(conf) {
+    for (const name in conf) {
+        var mod = modules[name];
+        var args = conf[name];
+
+        if (typeof mod !== 'function')
+            throw new Error('Invalid module: ' + name);
+        mod(args);
+    }
+}
+
+function dependency_check(deps) {
+    for (var name in deps) {
+        var objects = deps[name];
         for (var o in objects) {
             var type = objects[o];
             try {
@@ -20,115 +37,24 @@ function module_check(modules) {
     }
 }
 
-module_check({
+dependency_check({
     'jquery': {$: 'function'}
 });
 
 
-function init_overflow_handler(
-    axis = 'xy', child = $(document.body), parent = $(window),
-    attr_overflow = 'data-overflow',
-    fn = undefined, rfn = undefined
-) {
-    function handle_overflow(
-        parent, child, axis,
-        fn, rfn
-    ) {
-        function overflow(parent, child, axis)
-        {
-            switch (axis) {
-                case 'x':
-                    return child.prop('scrollWidth') >
-                        child.prop('clientWidth');
-                case 'y':
-                    return child.prop('scrollHeight') >
-                        child.prop('clientHeight');
-                case 'xy':
-                    return overflow(parent, child, 'x') ||
-                        overflow(parent, child, 'y');
-                default:
-                    break;
-            }
+modules['dom-defaults'] = function (args)
+{
+    var args = module_args(args, {
+        defaults: {}
+    });
 
-            return false;
-        }
-
-        var overflow_thres = undefined;
-
-        function overflow_fn() {
-            if (overflow(parent, child, axis)) {
-                overflow_thres = overflow_thres !== undefined ?
-                    Math.max(overflow_thres, child.width()) : child.width();
-                return fn(child);
-            }
-
-            if (overflow_thres !== undefined &&
-                child.width() <= overflow_thres)
-                return;
-            return rfn(child);
-        }
-
-        overflow_fn();
-        parent.on('resize', overflow_fn);
-    }
-
-    return handle_overflow(
-        parent, child, axis,
-        function (o) {
-            o.attr(attr_overflow, '');
-            if (fn !== undefined) return fn(o);
-        },
-        function (o) {
-            o.removeAttr(attr_overflow);
-            if (rfn !== undefined) return fn(o);
-        }
-    );
-}
-
-function init_target_index(
-    attr_target_index = '[data-target-index]'
-) {
-    if ($(':target').length === 0) {
-        location.replace('#' + $(attr_target_index).attr('id'));
-    }
-}
-
-function init_markdown(
-    renderers, default_renderer,
-    attr_renderer = 'format',
-    attr_md = 'md', attr_md_src = 'src'
-) {
-    function render_markdown(o, text, renderer) {
-        return o.html(eval(renderers[renderer])(text));
-    }
-
-    $(attr_md).each(
-        function () {
-            var o = $(this);
-            var src = o.attr(attr_md_src);
-            var renderer = o.attr(attr_renderer);
-            if (typeof renderer === 'undefined' || renderer === false)
-                renderer = default_renderer;
-
-            if (typeof src === 'undefined' || src === false) {
-                render_markdown(o, o.html(), renderer);
-            } else {
-                $.get(src, function (data) {
-                    render_markdown(o, data, renderer);
-                });
-            }
-        }
-    );
-}
-
-function init_dom_defaults(defaults) {
     function has_attr(o, name) {
         const attr = o.attr(name);
         return typeof attr !== 'undefined' && attr !== false;
     }
 
-    for (const element in defaults) {
-        const attrs = defaults[element];
+    for (const element in args['defaults']) {
+        const attrs = args['defaults'][element];
         $(element).each(
             function () {
                 const o = $(this);
@@ -139,4 +65,49 @@ function init_dom_defaults(defaults) {
             }
         );
     }
-}
+};
+
+modules['anchor-target'] = function (args)
+{
+    var args = module_args(args, {
+        target: $('[data-target-index]')
+    });
+
+    if ($(':target').length === 0) {
+        location.replace('#' + $(args['target']).attr('id'));
+    }
+};
+
+modules['markdown'] = function (args)
+{
+    const attr_renderer = 'format',
+          attr_md = 'md',
+          attr_md_src = 'src';
+
+    var args = module_args(args, {
+        renderers: undefined,
+        default_renderer: undefined
+    });
+
+    function render_markdown(o, text, renderer) {
+        return o.html(eval(args['renderers'][renderer])(text));
+    }
+
+    $(attr_md).each(
+        function () {
+            var o = $(this);
+            var src = o.attr(attr_md_src);
+            var renderer = o.attr(attr_renderer);
+            if (typeof renderer === 'undefined' || renderer === false)
+                renderer = args['default_renderer'];
+
+            if (typeof src === 'undefined' || src === false) {
+                render_markdown(o, o.html(), renderer);
+            } else {
+                $.get(src, function (data) {
+                    render_markdown(o, data, renderer);
+                });
+            }
+        }
+    );
+};
--- assets/ui.js
+++ assets/ui.js
@@ -1,35 +1,133 @@
 'use strict';
 console.assert(typeof $ === 'function');
 
+var modules;
 
-function set_ui_toggleable(
-    o, attr_checked = 'data-checked',
-    attr_onchecked = 'data-onchecked', attr_onunchecked = 'data-onunchecked',
-    ev_checked = 'toggle:checked', ev_unchecked = 'toggle:unchecked'
-) {
-    function set_state(o, checked) {
-        if (checked) {
-            o.attr(attr_checked, '');
-            o.trigger(ev_checked);
-            eval(o.attr(attr_onchecked));
-        } else {
-            o.removeAttr(attr_checked);
-            o.trigger(ev_unchecked);
-            eval(o.attr(attr_onunchecked));
+function ui_bistate_dispatch(o, state, args)
+{
+    switch (state) {
+        case true:
+            o.attr(args['attr'], '');
+            if (args['ev']['on'] !== undefined)
+                o.trigger(args['ev']['on']);
+            eval(o.attr(args['ev_attr']['on']));
+            break;
+        case false:
+            o.removeAttr(args['attr']);
+            if (args['ev']['off'] !== undefined)
+                o.trigger(args['ev']['off']);
+            eval(o.attr(args['ev_attr']['off']));
+            break;
+        default:
+            break;
+    }
+}
+
+
+modules['toggle-switch'] = function (args)
+{
+    const ui_toggleable = '.ui-toggle';
+    const ui_attr_checked = 'data-checked';
+    const ui_dispatch_opts = {
+        attr: ui_attr_checked,
+        ev: {
+            on: 'toggle:checked',
+            off: 'toggle:unchecked'
+        },
+        ev_attr: {
+            on: 'data-onchecked',
+            off: 'data-onunchecked'
         }
+    };
+
+    var args = module_args(args, {
+        target: $(ui_toggleable)
+    });
+
+    function set_state(o, checked) {
+        ui_bistate_dispatch(o, checked, ui_dispatch_opts);
     }
 
     function get_state(o) {
-        var checked = o.attr(attr_checked);
+        var checked = o.attr(ui_attr_checked);
         return typeof checked !== 'undefined' && checked !== false;
     }
 
-    set_state(o, get_state(o));
-    o.on('click', function () {set_state($(this), !get_state($(this)));});
-}
+    set_state(args['target'], get_state(args['target']));
+    args['target'].on('click', function () {
+        set_state($(this), !get_state($(this)));
+    });
+};
 
-function init_toggle_button(
-    attr_toggle_button = '.ui-toggle'
-) {
-    return set_ui_toggleable($(attr_toggle_button));
-}
+modules['overflow'] = function (args)
+{
+    const ui_dispatch_opts = {
+        attr: 'data-overflow',
+        ev: {
+            on: 'responsive:overflow',
+            off: 'responsive:unoverflow'
+        },
+        ev_attr: {
+            on: 'data-onoverflow',
+            off: 'data-offoverflow'
+        }
+    };
+
+    var args = module_args(args, {
+        axis: 'xy',
+        child: $(document.body),
+        parent: $(window)
+    });
+
+    function handle_overflow(
+        parent, child, axis,
+        fn, rfn
+    ) {
+        function overflow(parent, child, axis)
+        {
+            switch (axis) {
+                case 'x':
+                    return child.prop('scrollWidth') >
+                        child.prop('clientWidth');
+                case 'y':
+                    return child.prop('scrollHeight') >
+                        child.prop('clientHeight');
+                case 'xy':
+                    return overflow(parent, child, 'x') ||
+                        overflow(parent, child, 'y');
+                default:
+                    break;
+            }
+
+            return false;
+        }
+
+        var overflow_thres = undefined;
+
+        function overflow_fn() {
+            if (overflow(parent, child, axis)) {
+                overflow_thres = overflow_thres !== undefined ?
+                    Math.max(overflow_thres, child.width()) : child.width();
+                return fn(child);
+            }
+
+            if (overflow_thres !== undefined &&
+                child.width() <= overflow_thres)
+                return;
+            return rfn(child);
+        }
+
+        overflow_fn();
+        parent.on('resize', overflow_fn);
+    }
+
+    return handle_overflow(
+        args['parent'], args['child'], args['axis'],
+        function (o) {
+            return ui_bistate_dispatch(o, true, ui_dispatch_opts);
+        },
+        function (o) {
+            return ui_bistate_dispatch(o, false, ui_dispatch_opts);
+        }
+    );
+};
